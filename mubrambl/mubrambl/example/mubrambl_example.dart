e8cb67c8b149b0d449ecc2aac3e93e9d20d4a0a3
Sterling Wells <s.wells@topl.me>
2021-05-28 14:14:45 -0700

    Package-simple starter template from dart

---
+++ mubrambl/mubrambl/example/mubrambl_example.dart
@@ -0,0 +1,6 @@
+import 'package:mubrambl/mubrambl.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ mubrambl/mubrambl/lib/mubrambl.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library mubrambl;
+
+export 'src/mubrambl_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
