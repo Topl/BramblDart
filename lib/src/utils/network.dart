e9a0a29daed81de030a1f31893101241306c94d6
Sterling Wells <s.wells@topl.me>
2021-07-13 17:08:46 -0400

    Added network utility

---
+++ lib/src/utils/network.dart
@@ -0,0 +1,18 @@
+/// Container for information about topl networks
+///
+/// This could be used also for other than Torus networks when this library gets extended
+
+class Network {
+  static const toplTestNetPrivate = 0x40;
+  static const toplPublic = 0x01;
+  static const toplTestnetPublic = 0x10;
+
+  final bool testnet;
+  final int networkPrefix;
+
+  Network(this.testnet, this.networkPrefix);
+
+  factory Network.Toplnet() => Network(false, toplPublic);
+  factory Network.Valhalla() => Network(true, toplTestnetPublic);
+  factory Network.Private() => Network(true, toplTestNetPrivate);
+}
