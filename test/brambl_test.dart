0a441c0642e3d75dae6b47924f498f8398cb404e
Sterling Wells <s.wells@topl.me>
2021-07-15 13:29:47 -0400

    Added the ability for BramblDart to generate addresses

--- test/brambl_test.dart
+++ test/brambl_test.dart
@@ -145,42 +145,59 @@ void main() {
     });
   });
 
-  group('test key utilities', () {
-    // test key recovery success
-    test('keyRecovery success', () {
-      final cipherParams = CipherParams('NPL8XoWBJL1x539wb19jL8');
-      final crypto = Crypto(
-          '2DbmnQiaUyBrgZka4ZhrGibop7gf6wQsLtTfFeTVQkYv',
-          'scrypt',
-          '3zHMnaVUNqcbLqKdA9G5EWy2CDRmvvDZZjW7FBChdM3ru6UJWxDECNPqchuNDjyyTrmFGSRN2m34NeDD8oL1PiUn',
-          '7Z7Z99siQvzRUdcX9SH1s45F4mMsYSa3YYXFq7Tqq5Ns',
-          'aes-256-ctr',
-          cipherParams);
-      final address = '3NKunrdkLG6nEZ5EKqvxP5u4VjML3GBXk2UQgA9ad5Rsdzh412Dk';
-      final kdfParams = KDFParams(32, 262144, 8, 1);
+  group('test generatePubKeyHashAddress', () {
+    // testing the generation of address using the hash of the public key
 
-      final keys = recover('test', KeyFile(crypto, address), kdfParams);
-      expect(keys, [
-        'DvovrPBee6AVQKaA7Ldd6ZSmvFaXptXadntrjeSCjroE',
-        '3L92EtcUV6Eh8G5A9iBnFhitLuTdzeZ814SuMD5dzDqv'
-      ]);
+    // test generate address for valid network, valid propositionType, valid publicKey
+    // valhalla
+    test('generatePubKeyHashAddress success', () {
+      final addressResult = generatePubKeyHashAddress(
+          str2ByteArray('3L92EtcUV6Eh8G5A9iBnFhitLuTdzeZ814SuMD5dzDqv'),
+          'valhalla',
+          'PublicKeyCurve25519');
+      expect(addressResult['success'], true);
+      expect(addressResult['address'],
+          '3NKunrdkLG6nEZ5EKqvxP5u4VjML3GBXk2UQgA9ad5Rsdzh412Dk');
     });
 
-    // throws exception if password is incorrect (anagram)
-    test('keyRecovery incorrect anagram', () {
-      final cipherParams = CipherParams('NPL8XoWBJL1x539wb19jL8');
-      final crypto = Crypto(
-          '2DbmnQiaUyBrgZka4ZhrGibop7gf6wQsLtTfFeTVQkYv',
-          'scrypt',
-          '3zHMnaVUNqcbLqKdA9G5EWy2CDRmvvDZZjW7FBChdM3ru6UJWxDECNPqchuNDjyyTrmFGSRN2m34NeDD8oL1PiUn',
-          '7Z7Z99siQvzRUdcX9SH1s45F4mMsYSa3YYXFq7Tqq5Ns',
-          'aes-256-ctr',
-          cipherParams);
-      final address = '3NKunrdkLG6nEZ5EKqvxP5u4VjML3GBXk2UQgA9ad5Rsdzh412Dk';
-      final kdfParams = KDFParams(32, 262144, 8, 1);
+    // test generate address for valid network, valid propositionType, valid publicKey
+    // private
+    test('generatePubKeyHashAddress success private', () {
+      final addressResult = generatePubKeyHashAddress(
+          str2ByteArray('GFcygo2bL7VErTNaxMekDyNv4ME3EWtSBH3xjogYHS7n'),
+          'private',
+          'PublicKeyCurve25519');
+      expect(addressResult['success'], true);
+      expect(addressResult['address'],
+          'AU9xd9iQ8JHz9dUKiYnDXoPFwahoonUGQHbbSG6SW1ZXu4K7nbx7');
+    });
 
-      expect(() => recover('estt', KeyFile(crypto, address), kdfParams),
-          throwsA(TypeMatcher<ArgumentError>()));
+    // test generate address for valid network, valid propositionType, invalid publicKey
+    test('generatePubKeyHashAddress failure invalidPublicKey', () {
+      final addressResult = generatePubKeyHashAddress(
+          str2ByteArray('GFcygo2bL7VErTNaxMekDyNv4ME3EWtSBH3xjog'),
+          'private',
+          'PublicKeyCurve25519');
+      expect(addressResult['success'], false);
+      expect(addressResult['errorMsg'], 'Invalid publicKey length');
+    });
+
+    // test generate address for invalid network, valid propositionType, invalid publicKey
+    test('generatePubKeyHashAddress failure invalid network', () {
+      final addressResult = generatePubKeyHashAddress(
+          str2ByteArray('GFcygo2bL7VErTNaxMekDyNv4ME3EWtSBH3xjog'),
+          'privacy',
+          'PublicKeyCurve25519');
+      expect(addressResult['success'], false);
+    });
+
+    // test generate address for valid network, invalid propositionType, invalid publicKey
+    test('generatePubKeyHashAddress failure invalid network', () {
+      final addressResult = generatePubKeyHashAddress(
+          str2ByteArray('GFcygo2bL7VErTNaxMekDyNv4ME3EWtSBH3xjog'),
+          'private',
+          'PublicKeyCurve25518');
+      expect(addressResult['success'], false);
     });
   });
 }
