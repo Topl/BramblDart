02250203aa52a4cfdaddc5bb58d6b9fd21238e3d
Sterling Wells <s.wells@topl.me>
2021-07-23 17:38:28 -0500

    Imported some missing packages

--- lib/src/HD/keygen.dart
+++ lib/src/HD/keygen.dart
@@ -1,3 +1,10 @@
+import 'dart:typed_data';
+
+import 'package:pointycastle/digests/sha512.dart';
+import 'package:pointycastle/key_derivators/api.dart';
+import 'package:pointycastle/key_derivators/pbkdf2.dart';
+import 'package:pointycastle/macs/hmac.dart';
+
 /// Create a new seed from entropy and password
 ///
 /// The output size of pbkdf2 is associated with the size of the slice, allowing
@@ -5,7 +12,7 @@
 
 Uint8List generateSeed(Uint8List entropy, Uint8List password) {
   const ITER = 4096;
-  final KeyDerivator = PBKDF2KeyDerivator(HMac(SHA512Digest(), 64);
+  final keyDerivator = PBKDF2KeyDerivator(HMac(SHA512Digest(), 64));
   final params = Pbkdf2Parameters(entropy, ITER, 32);
   keyDerivator.init(params);
   return keyDerivator.process(password);
