c113c1b629f410f654c420aa2cd0741b245ea02b
Sterling Wells <s.wells@topl.me>
2021-07-26 16:11:40 -0500

    Added some additional documentation

--- lib/src/models/x_prv.dart
+++ lib/src/models/x_prv.dart
@@ -2,10 +2,20 @@ import 'dart:typed_data';
 
 import 'package:bip32_ed25519/bip32_ed25519.dart';
 
+/// HD extended private key
+///
+/// Effectively this is ed25519 extended secret key (64 bytes) followed by a chain code (32 bytes)
 class XPrv {
   final Uint8List bytes;
+
+  // Create a XPrv from the given bytes.
+  //
+  // This function does not perform any validity check and should not be used outside
+  // of this module.
   XPrv(this.bytes);
 
+  /// takes the given raw bytes and perform some modifications to normalize
+  /// it properly to a XPrv using the specification in the SLIP-0023 proposal.
   factory XPrv.normalize_bytes(Uint8List bytes) {
     bytes[0] = bytes[0] & 0xF8;
     bytes[31] = bytes[31] & 0x1F;
