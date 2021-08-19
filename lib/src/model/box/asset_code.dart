0bceda49c6ff5ea71fb36c859ce0b15797aaab4b
Sterling Wells <s.wells@topl.me>
2021-08-19 11:20:14 -0400

    Asset Code Class

---
+++ lib/src/model/box/asset_code.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:mubrambl/src/credentials/address.dart';
+import 'package:mubrambl/src/encoding/base_58_encoder.dart';
+import 'package:mubrambl/src/utils/constants.dart';
+import 'package:mubrambl/src/utils/extensions.dart';
+import 'package:mubrambl/src/utils/util.dart';
+
+class AssetCode {
+  int assetCodeVersion;
+  final ToplAddress issuer;
+  String shortName;
+  String networkPrefix;
+
+  AssetCode(
+      this.assetCodeVersion, this.issuer, this.shortName, this.networkPrefix);
+
+  factory AssetCode.initialize(
+      int version, ToplAddress issuer, String name, String networkPrefix) {
+    if (!isValidNetwork(networkPrefix)) {
+      throw ArgumentError('Invalid network provided');
+    }
+    assert(version == 1, 'AssetCode version required to be 1');
+    assert(name.length <= SHORT_NAME_LIMIT,
+        'Asset short names must be less than 8 Latin-1 encoded characters');
+    if (name.getValidLatinBytes() == null) {
+      throw Exception('String is not valid Latin-1');
+    }
+    final validationResult =
+        validateAddressByNetwork(networkPrefix, issuer.toBase58());
+    if (!validationResult['success']) {
+      throw ArgumentError('Invalid Issuer Address:: Network Type: <' +
+          networkPrefix +
+          '>' +
+          ' Invalid Address: <' +
+          issuer.toBase58() +
+          '>');
+    }
+    return AssetCode(version, issuer, name, networkPrefix);
+  }
+
+  ///
+  /// @param {string} networkPrefix prefix of network where address will be used
+  /// @returns {string} return asset code
+  String serialize(String networkPrefix) {
+    final addressBytes = issuer.buffer.asUint8List();
+    final slicedAddress = addressBytes.sublist(0, 34);
+
+    // concat 01 [version] + 34 bytes [address] + ^8bytes [asset name]
+    final version = Uint8List.fromList([0x01]);
+    final concatValues = version +
+        slicedAddress +
+        latin1.encode(shortName.padLeft(
+            SHORT_NAME_LIMIT)); // add trailing zeros, shortname must be 8 bytes long
+    return Base58Encoder.instance.encode(concatValues);
+  }
+}
--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -2,3 +2,5 @@ const XPRV_SIZE = 96;
 const XPUB_SIZE = 64;
 const SEED_SIZE = 32;
 const MAX_MNEMONIC_VALUE = 2047;
+const MAX_JS = 52;
+const SHORT_NAME_LIMIT = 8;
