9120ea529cd853ab4c96fe6cbd3d2a12ccf3c275
Kenneth Sterling Wells <72569701+sterlingwellscaffeine@users.noreply.github.com>
2021-09-02 16:24:01 -0400

    Added modifierId class (#57)
    Co-authored-by: Sterling Wells <s.wells@topl.me>

---
+++ lib/src/modifier/modifier_id.dart
@@ -0,0 +1,41 @@
+import 'package:collection/collection.dart';
+import 'package:mubrambl/src/utils/constants.dart';
+import 'package:mubrambl/src/utils/string_data_types.dart';
+import 'package:pinenacl/ed25519.dart';
+import 'package:mubrambl/src/utils/codecs/string_data_types_codec.dart';
+
+typedef ModifierTypeId = int;
+
+class ModifierId extends ByteList {
+  ModifierId(Uint8List value) : super(value);
+
+  factory ModifierId.create(Uint8List value) {
+    assert(value.length == 1 + BLAKE2B_256_DIGEST_SIZE);
+    return ModifierId(value);
+  }
+
+  factory ModifierId.empty() {
+    return ModifierId(Uint8List(1 + BLAKE2B_256_DIGEST_SIZE));
+  }
+
+  factory ModifierId.fromBase58(Base58Data data) {
+    return ModifierId(data.value);
+  }
+
+  List<int> get getIdBytes => buffer.asUint8List().sublist(1);
+
+  ModifierTypeId get getModType => buffer.asUint8List()[0];
+
+  @override
+  int get hashCode => buffer.asByteData().getInt64(0);
+
+  @override
+  bool operator ==(Object other) =>
+      other is ModifierId &&
+      ListEquality().equals(buffer.asUint8List(), other.buffer.asUint8List());
+
+  @override
+  String toString() {
+    return buffer.asUint8List().encodeAsBase58().show;
+  }
+}
