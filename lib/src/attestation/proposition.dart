087407f6ff53a3eb755515d1d7b11adf956546b3
Sterling Wells <s.wells@topl.me>
2021-09-08 10:29:23 -0500

    Added proposition

---
+++ lib/src/attestation/proposition.dart
@@ -0,0 +1,38 @@
+import 'package:collection/collection.dart';
+import 'package:mubrambl/src/utils/errors.dart';
+import 'package:mubrambl/src/utils/string_data_types.dart';
+import 'package:pinenacl/x25519.dart';
+import 'package:mubrambl/src/utils/codecs/string_data_types_codec.dart';
+
+/// Propositions are challenges that must be satisfied by the prover.
+/// In most cases, propositions are used by transactions issuers (spenders) to prove the right
+/// to use a UTXO in a transaction.
+///
+class Proposition extends ByteList {
+  Proposition(Uint8List value) : super(value);
+
+  factory Proposition.fromString(String str) {
+    try {
+      return Proposition.fromBase58(Base58Data.validated(str));
+    } catch (e) {
+      throw IncorrectEncoding('String is an incorrect encoding type: $e');
+    }
+  }
+
+  factory Proposition.fromBase58(Base58Data data) {
+    return Proposition(data.value);
+  }
+
+  @override
+  String toString() {
+    return buffer.asUint8List().encodeAsBase58().show;
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      other is Proposition &&
+      ListEquality().equals(buffer.asUint8List(), other.buffer.asUint8List());
+
+  @override
+  int get hashCode => buffer.hashCode;
+}
