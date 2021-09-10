cd20129b5604e5d506bc9d5bf3661ed51a3617fc
Sterling Wells <shoshonte@gmail.com>
2021-09-10 13:29:07 -0500

    Deleted files that will be checked in next PR

--- lib/src/modifier/block/block_body.dart
+++
@@ -1,9 +0,0 @@
-// import '../modifier_id.dart';
-
-// class BlockBody {
-//   final ModifierId id;
-//   final ModifierId parentId;
-//   final 
-
-//   ModifierTypeId get modifierTypeId => 5;
-// }
