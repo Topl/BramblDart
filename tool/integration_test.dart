0abd9008f3d74a1827e8bb1e753ec1c788dea940
Sterling Wells <shoshonte@gmail.com>
2021-09-14 09:52:43 -0500

    Added first node information routes

--- tool/integration_test.dart
+++ tool/integration_test.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:http/http.dart';
+import 'package:mubrambl/src/core/amount.dart';
 import 'package:mubrambl/src/core/client.dart';
+import 'package:mubrambl/src/credentials/address.dart';
 import 'package:test/test.dart';
 
 import 'test_api_key_auth.dart';
@@ -38,12 +40,48 @@ void main() {
   group(BramblClient, () {
     test('test node info on valhalla', () async {
       try {
-        var response = await client.getNodeInfo();
-        print(response.toString());
+        var response = await client.getClientVersion();
+        print(response);
       } catch (e) {
         print(e);
         fail('exception: $e');
       }
     });
-  });
+
+    test('test node info on valhalla', () async {
+      try {
+        var response = await client.getNetwork();
+        print(response);
+      } catch (e) {
+        print(e);
+        fail('exception: $e');
+      }
+    });
+
+    test('test block head info on Valhalla', () async {
+      try {
+        var response = await client.getBlockNumber();
+        print(response);
+      } catch (e) {
+        print(e);
+        fail('exception: $e');
+      }
+    });
+
+    test('topl get balance', () async {
+      final balance = await client.getBalance(ToplAddress.fromBase58(
+          '3NKunrdkLG6nEZ5EKqvxP5u4VjML3GBXk2UQgA9ad5Rsdzh412Dk'));
+      expect(
+          BigInt.from((balance['Polys'] as PolyAmount).getInNanopoly) <=
+              BigInt.parse('170141183460469231731687303715884105727'),
+          isTrue);
+      expect(
+          BigInt.from((balance['Arbits'] as ArbitAmount).getInNanoarbit) <=
+              BigInt.parse('170141183460469231731687303715884105727'),
+          isTrue);
+    });
+  },
+      skip: baasProjectId == '' || baasProjectId.length != 24
+          ? 'Tests require a valid BaaS projectId'
+          : null);
 }
