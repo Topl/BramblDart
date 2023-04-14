ef8066e851007f7d5c474cdb3e2e643bbaf70d6f
Selby Baidjnath <selby@serubii.com>
2023-04-14 16:48:09 -0300

    swapped contents of test main and example main

--- example/brambl_dart_example.dart
+++ example/brambl_dart_example.dart
@@ -1,2 +1,16 @@
 
-void main() {}
+import 'package:brambl_dart/brambl_dart.dart';
+import 'package:brambl_dart/src/utils/extensions.dart';
+
+void main() {
+
+
+  /// encode String to blake2b256
+  ///
+
+  final input = "Foobar";
+
+  final byteArray = Blake2b256().hash([input.toUtf8Uint8List()]);
+  print(byteArray.toHexString());
+
+}
