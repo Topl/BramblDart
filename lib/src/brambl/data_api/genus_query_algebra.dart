37c1d10b3d4446171f78197ae432a8c4125dffc5
Selby Baidjnath <selby@serubii.com>
2023-07-21 10:59:15 -0300

    Data api genus query algebra (#103)
    First SDK PR is the DataAPI GenusQueryAlgebra
    
    requires merge of https://github.com/Topl/BramblDart/pull/102 first.
    don't review before that one is merged in.

---
+++ lib/src/brambl/data_api/genus_query_algebra.dart
@@ -0,0 +1,24 @@
+import 'package:grpc/grpc.dart';
+import 'package:topl_common/proto/brambl/models/address.pb.dart';
+import 'package:topl_common/proto/genus/genus_models.pb.dart';
+import 'package:topl_common/proto/genus/genus_rpc.pbgrpc.dart';
+
+/// Defines a Genus Query API for interacting with a Genus node.
+class GenusQueryAlgebra {
+  final ClientChannel channel;
+
+  GenusQueryAlgebra(this.channel);
+
+  /// Query and retrieve a set of UTXOs encumbered by the given LockAddress.
+  ///
+  /// [fromAddress] The lock address to query the unspent UTXOs by.
+  /// [txoState] The state of the UTXOs to query. By default, only unspent UTXOs are returned.
+  /// returns A sequence of UTXOs.
+  Future<List<Txo>> queryUtxo({required LockAddress fromAddress, TxoState txoState = TxoState.UNSPENT}) async {
+    final blockingStub = TransactionServiceClient(channel);
+    final response = await blockingStub.getTxosByAddress(
+      QueryByAddressRequest(address: fromAddress, confidenceFactor: null, state: txoState),
+    );
+    return response.txos;
+  }
+}
