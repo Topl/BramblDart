1c220f9b311a7d83c6b4150e2d25b4304d4cf580
Selby Baidjnath <selby@serubii.com>
2023-10-20 19:43:10 -0300

    remove dart kit and it's dependencies

--- lib/src/service_kit/definitions/wallet_key_api_definition.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-
-abstract class WalletKeyApiDefinition {
-  Future<void> updateMainKeyVaultStore(String mainKeyVaultStore, String name);
-  Future<void> deleteMainKeyVaultStore(String name);
-  Future<void> saveMainKeyVaultStore(String mainKeyVaultStore, String name);
-  Future<String> getMainKeyVaultStore(String name);
-  Future<void> saveMnemonic(List<String> mnemonic, String mnemonicName);
-}
