14291fa0c6bef33ba6da8af3ad68019ac8942f87
Selby Baidjnath <selby@serubii.com>
2023-11-07 15:01:27 -0300

    Add constants file to export

--- lib/src/brambl/brambl.dart
+++ lib/src/brambl/brambl.dart
@@ -4,27 +4,22 @@ library brambldart.brambl;
 
 /// --- Builders ---
 export 'builders/builders.dart';
-
 /// --- Codecs ---
 export 'codecs/address_codecs.dart';
-
 /// --- Common ---
 export 'common/common.dart';
-
+/// --- Constants ---
+export 'constants/network_constants.dart';
 /// --- Brambl ---
 export "context.dart";
-
 /// --- Data API ---
 export 'data_api/data_api.dart';
-
 /// --- Syntax ---
 export 'syntax/syntax.dart';
-
 /// --- Utils ---
 export 'utils/encoding.dart';
 export 'utils/grpc/grpc.dart';
 export 'utils/proto_converters.dart';
 export 'wallet/credentialler.dart';
-
 /// --- Wallet API ---
 export 'wallet/wallet_api.dart';
