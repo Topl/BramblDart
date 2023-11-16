6bf6844955adef8b7713400af424d8d76ab3f2ee
Selby Baidjnath <selby@serubii.com>
2023-11-16 13:42:15 -0300

    solve parameter  issue

--- lib/src/crypto/generation/mnemonic/language.dart
+++ lib/src/crypto/generation/mnemonic/language.dart
@@ -44,55 +44,55 @@ class ChineseSimplified extends Language {
 
 class ChineseTraditional extends Language {
   const ChineseTraditional()
-      : super(Languages.chineseTraditional,
+      : super(Languages.chineseTraditional, 'chinese_traditional.txt',
             '85b285c4e0e3eb1e52038e2cf4b4f8bba69fd814e1a09e063ce3609a1f67ad62');
 }
 
 class English extends Language {
   const English()
-      : super(Languages.english,
+      : super(Languages.english,  'english.txt',
             'ad90bf3beb7b0eb7e5acd74727dc0da96e0a280a258354e7293fb7e211ac03db');
 }
 
 class French extends Language {
   const French()
-      : super(Languages.french,
+      : super(Languages.french, 'french.txt',
             '9cbdaadbd3ce9cbaee1b360fce45e935b21e3e2c56d9fcd56b3398ced2371866');
 }
 
 class Italian extends Language {
   const Italian()
-      : super(Languages.italian,
+      : super(Languages.italian, 'italian.txt',
             '80d2e90d7436603fd6e57cd9af6f839391e64beac1a3e015804f094fcc5ab24c');
 }
 
 class Japanese extends Language {
   const Japanese()
-      : super(Languages.japanese,
+      : super(Languages.japanese, 'japanese.txt',
             'd9d1fde478cbeb45c06b93632a487eefa24f6533970f866ae81f136fbf810160');
 }
 
 class Korean extends Language {
   const Korean()
-      : super(Languages.korean,
+      : super(Languages.korean, 'korean.txt',
             'f04f70b26cfef84474ff56582e798bcbc1a5572877d14c88ec66551272688c73');
 }
 
 class Spanish extends Language {
   const Spanish()
-      : super(Languages.spanish,
+      : super(Languages.spanish, 'spanish.txt',
             'a556a26c6a5bb36db0fb7d8bf579cb7465fcaeec03957c0dda61b569962d9da5');
 }
 
 class Czech extends Language {
   const Czech()
-      : super(Languages.czech,
+      : super(Languages.czech, 'czech.txt',
             'f9016943461800f7870363b4c301c814dbcb8f4de801e6c87d859eba840469d5');
 }
 
 class Portuguese extends Language {
   const Portuguese()
-      : super(Languages.portuguese,
+      : super(Languages.portuguese, 'portuguese.txt',
             'eed387d44cf8f32f60754527e265230d8019e8a2277937c71ef812e7a46c93fd');
 }
 
